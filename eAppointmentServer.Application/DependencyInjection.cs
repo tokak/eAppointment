37a234afa0c6ef490f686345f30e5ec7ac15c91f
Murat Tokak <tokakmurat01@gmail.com>
2025-11-04 01:55:49 +0300

    Kimlik doğrulama ve yetkilendirme eklendi Token tabanlı giriş, rol yönetimi ve API endpoint güvenliği sağlanarak güvenli kullanıcı yönetimi oluşturuldu.

---
+++ eAppointmentServer.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace eAppointmentServer.Application
+{
+    public static class DependencyInjection
+    {
+        // Uygulama katmanındaki MediatR servislerini otomatik olarak projeye ekler.
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            });
+            return services;
+        }
+    }
+}
