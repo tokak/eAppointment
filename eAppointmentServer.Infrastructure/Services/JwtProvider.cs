25410d957ceb9c1cb4e84b846d68e6a61a1a7b29
Murat Tokak <tokakmurat01@gmail.com>
2025-11-05 12:29:17 +0300

    Infrastructure katmanı işlemlerinin tamamlanması

---
+++ eAppointmentServer.Infrastructure/Services/JwtProvider.cs
@@ -0,0 +1,13 @@
+﻿using eAppointmentServer.Application.Services;
+using eAppointmentServer.Domain.Entities;
+
+namespace eAppointmentServer.Infrastructure.Services
+{
+    internal sealed class JwtProvider : IJwtProvider
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
